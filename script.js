5d77d5538c54bca295101dac314ce9fd8e8b38e8
c8R9z <christopher.valdma@tptlive.ee>
2023-10-11 13:19:24 +0300

    More fruits

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 for(let i=0; i<10; i++){
     console.log(i);
 }
+let i = 1;
 while(i<1_000_000){
     console.log(i)
     i*=2
+}
+
+i = 1;
+while(i>10){
+    console.log('somestuff')
+}
+
+i = 1;
+do {
+    console.log('somestuff')
+} while(i>10);
+
+let fruits=['banana', 'cherry', 'apple', 'pear'];
+
+for (let i=0; i<fruits.length; i++){
+    console.log(fruits[i])
 }
