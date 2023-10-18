5aea4d816ee7a113b7a3a0d89e04a3aa3e933836
c8R9z <christopher.valdma@tptlive.ee>
2023-10-18 13:04:01 +0300

    Fun stuff in terminal

--- script.js
+++ script.js
@@ -33,3 +33,41 @@ for(const property in object){
     console.log(property);
     console.log(object[property])
 }
+
+let num = 10;
+if(num > 10){
+    console.log('suurem');
+} else if(num === 10){
+    console.log('võrdne')
+} else {
+    console.log('väiksem')
+}
+
+let day = new Date('2024-05-19').getDay();
+console.log(day);
+
+switch(day){
+    case 0:
+        console.log('Pühapäev')
+        break;
+    case 1:
+        console.log('Esmaspäev')
+        break;
+    case 2:
+        console.log('Teisipäev')
+        break;
+    case 3:
+        console.log('Kolmapäev')
+        break;
+    case 4:
+        console.log('Neljapäev')
+        break;
+    case 5:
+            console.log('Reede')
+        break;
+    case 6:
+        console.log('Laupäev')
+        break;
+    default:
+        console.log('Weirdday')
+}
