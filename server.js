a428d14e9e0e6413656f989ce8e2ccc03cb492ab
Christopher Valdma <christopher.valdma@tptlive.ee>
2024-01-24 13:23:02 +0200

    .

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const app = express();
+const port = 3000;
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+
+app.get('/', (reg, res) => {
+    res.render('index.njk');
+});
+
+app.get('/page2', (reg, res) => {
+    res.render('page2.njk');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`);
+})
