058cdf32b44dedbae52ac22e24113831fd127e1a
Christopher Valdma <christopher.valdma@tptlive.ee>
2024-01-31 14:21:17 +0200

    .

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
 const express = require('express');
-const nunjucks = require('nunjucks');
+const nunjucks = require(`nunjucks`);
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({
+  extended:true
+}))
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
-
-
 app.get('/', (req, res) => {
-    res.render('index.njk');
+  res.render('index.njk');
+  //console.log (`somebody visited`);
+});
+app.get(`/page2`, (req, res)=> {
+    res.render(`page2.njk`);
+});
+app.get(`/form`, (req, res)=> {
+  console.log(req.query)
+  res.render(`form.njk`, req.query);
+});
+app.get(`/circle`, (req, res)=> {
+  console.log(req.query)
+  res.render(`circle.njk`, req.query);
+});
+app.post(`/circle`, (req, res)=> {
+  res.json(req.body);
+});
+app.get(`/circle`, (req, res)=> {
+  let area = Math.PI * req.body.radiuse
+  res.render(`circleAswere.njk`, {r: req.body.radiuse, a: area });
 });
 
-app.get('/page2', (req, res) => {
-    res.render('page2.njk');
-});
 
-app.get('/form', (req, res) => {
-    console.log(req.query)
-    res.render('form.njk', req.query);
-});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
