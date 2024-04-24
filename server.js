9324daa8643aa54fb9f872eff6c4ef4dafbfddc7
Christopher Valdma <christopher.valdma@tptlive.ee>
2024-04-24 14:59:01 +0300

    .

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/circle', (req, res) => {
     });
   });
 
-const movieController = require('./scr/movieController.js');
+const movieController = require('./src/movieController.js');
 app.use('/movies', movieController);
 
 
